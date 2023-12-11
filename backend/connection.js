c9cd4d3d95592c685ceb034eeb69ee09056a3271
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2023-12-11 17:50:31 +0530

    mongo atlas connect

--- backend/connection.js
+++ backend/connection.js
@@ -5,7 +5,7 @@ const uri = "mongodb://127.0.0.1:27017/mongodbPractice";
 
 const connect = () => {
     try {
-        mongoose.connect(uri);
+        mongoose.connect(process.env.URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("error connecting with database")
