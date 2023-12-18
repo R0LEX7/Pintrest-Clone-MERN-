5a93a899fbed4954e5bbda20f684f211ad5eab58
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2023-12-18 21:11:43 +0530

    cors policy

--- backend/index.js
+++ backend/index.js
@@ -34,12 +34,15 @@ app.use(
   })
 );
 
+// Example in Express.js
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
   next();
 });
+
 app.use(
   expressSession({
     resave: false,
