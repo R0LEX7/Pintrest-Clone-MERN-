ecc79aae83153caeae4cb4c61ffc6c07f115db47
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2024-01-11 02:55:54 +0530

    checking if work

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,19 @@ const { connect } = require("./connection.js");
 const userRouter = require("./routes/user.route.js");
 
 // middlewares
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use(cors());
 
 app.use(express.json());
